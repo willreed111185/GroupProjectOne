6215ff3c68760f9b9baac81d861210930af883d7
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-08-31 13:47:04 -0400

    corrected spelling of folder. deleted kk extra assets folder

---
+++ assets/javascript/tvapi.js
@@ -0,0 +1,24 @@
+
+const API_ROOT = "https://api.tvmaze.com/";
+const SHOWS_QUERY = "search/shows?q=";
+const SHOW_QUERY = "singlesearch/shows?q=";
+const PEOPLE_QUERY = "search/people?q=";
+
+
+
+<li data-key='19236'>
+	<div class='infobox'>
+		<ul class ='pricing-table'>
+			<li>
+				<div class='img'>
+					<a href ='/shows/19236/tvname'>
+						<img src='/static.tvmaze.com/uploads' alt='tvname'>
+					</a>
+					<li class='primary bullet-item'>
+						<a href='/shows/19236/tvname'>tvname</a>
+					</li>
+				</div>
+			</li>
+		</ul>
+	</div>
+</li>		
